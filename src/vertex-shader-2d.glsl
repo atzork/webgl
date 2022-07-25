dd780e0c65568d28f6952655a9fc359f5f23ffe1
zork <atzork@gmail.com>
2022-07-26 00:10:06 +0300

    scale

--- src/vertex-shader-2d.glsl
+++ src/vertex-shader-2d.glsl
@@ -3,11 +3,15 @@ attribute vec2 a_position;
 uniform vec2 u_resolution;
 uniform vec2 u_transition;
 uniform vec2 u_rotation;
+uniform vec2 u_scale;
 void main() {
+    // scaling
+    vec2 scaledPosition = a_position * u_scale;
+
     // Rotate the position
     vec2 rotatedPosition = vec2(
-        a_position.x * u_rotation.y + a_position.y * u_rotation.x,
-        a_position.y * u_rotation.y - a_position.x * u_rotation.x);
+    scaledPosition.x * u_rotation.y + scaledPosition.y * u_rotation.x,
+    scaledPosition.y * u_rotation.y - scaledPosition.x * u_rotation.x);
 
     // добавляем перенос
     vec2 position = rotatedPosition + u_transition;
